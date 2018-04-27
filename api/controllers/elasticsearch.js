30106bb781e28f3657e8d6561991907bcb57cea5
AdamOlsson <Olsson.95_@hotmail.com>
2018-04-27 14:04:13 +0200

    es path

--- api/controllers/elasticsearch.js
+++ api/controllers/elasticsearch.js
@@ -1,7 +1,44 @@
+var elasticsearch = require('elasticsearch');
+var schedule = require('node-schedule');
+
+var rule = new schedule.RecurrenceRule();
+rule.minute = 30;
+
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+
+var checkEsStatus = schedule.scheduleJob(rule, function(){
+    client.ping(
+        {
+        requestTimeout: 3000
+        },
+        (error) => {
+            if (error){
+                console.trace('Elasticsearch cluster is down!');    
+            } else {
+                console.log('Elasticsearch cluster is okay.') // TODO: UPLOAD DATA
+            }
+        }
+    
+    );
+});
 
 
 exports.get_test = (req, res, next) => {
     res.status(200).json({
         message: "Successful GET"
+    });
+}
+
+exports.post = (req, res, next) => {
+    client.index({
+        index: '',
+        type: '',
+        body: req.body
+    }, (error, response) => {
+        console.log(error);
     });
 }
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 const app = require("./app");
+const dgram = require("dgram")
+const io = require("socket.io")
 
 const port = process.env.PORT || 5000
+const udpSocket = dgram.createSocket("udp4")
+
+// Check dgram clusters
+udpSocket.bind({
+    address: "localhost",
+    port: 5001
+});
 
 const server = http.createServer(app);
 
